d0b393f3b7d34e3297a55590e94838dc0bbe3263
lin <ichenzhl@qq.com>
2024-04-02 12:05:13 +0800

    [test]: add test

--- model/example.model.go
+++ model/example.model.go
@@ -6,21 +6,24 @@ import (
 
 type TestA struct {
 	id               uint64 `bson:"_id"`
-	a                int64  `bson:"a"`
-	b                int32  `bson:"b"`
+	aaa              int64  `bson:"aaa"`
+	bbb              int32  `bson:"bbb"`
+	ccc              string `bson:"ccc"`
 	mdata.DirtyModel `bson:"-"`
 }
 type TestB struct {
 	id               uint64                      `bson:"_id"`
-	m                string                      `bson:"m"`
-	n                *TestA                      `bson:"n"` // 内嵌结构体类型 A
-	c                *mdata.MList[*TestA]        `bson:"c"`
-	d                *mdata.MMap[string, *TestA] `bson:"d"`
+	aa               string                      `bson:"aa"`
+	bb               *TestA                      `bson:"bb"` // 内嵌结构体类型 A
+	cc               *mdata.MList[*TestA]        `bson:"cc"`
+	dd               *mdata.MMap[string, *TestA] `bson:"dd"`
 	mdata.DirtyModel `bson:"-"`
 }
 type TestC struct {
-	id               uint64 `bson:"_id"`
-	x                string `bson:"x"`
-	y                *TestB `bson:"y"` // 内嵌结构体类型 B
+	id               uint64                      `bson:"_id"`
+	a                string                      `bson:"a"`
+	b                *TestB                      `bson:"b"` // 内嵌结构体类型 B
+	c                *mdata.MList[*TestB]        `bson:"c"`
+	d                *mdata.MMap[string, *TestB] `bson:"d"`
 	mdata.DirtyModel `bson:"-"`
 }
--- test/model_test.go
+++ test/model_test.go
@@ -9,58 +9,90 @@ import (
 	"gen_tools/test/mongo_helper"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"math"
 	"testing"
 )
 
+var mongoUrl = "mongodb+srv://ichenzhl:Qwert321@cluster0.feqwf3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //todo 换成自己的mongo地址测试
+
+func getTestC(id uint64) *model.TestC {
+	c := &model.TestC{}
+	c.SetId(id)
+	c.SetA("c_a")
+	c.SetB(&model.TestB{})
+	c.GetB().SetId(2)
+	c.GetB().SetAa("c_b_2")
+	c.GetB().SetBb(&model.TestA{})
+	c.GetB().GetBb().SetId(100)
+	c.GetB().GetBb().SetAaa(101)
+	c.GetB().GetBb().SetBbb(102)
+	c.GetB().GetBb().SetCcc("103")
+	c.GetB().SetCc(mdata.NewList[*model.TestA]())
+	c.GetB().GetCc().Append(&model.TestA{})
+	c.GetB().GetCc().Get(0).SetId(3)
+	c.GetB().GetCc().Get(0).SetAaa(4)
+	c.GetB().GetCc().Get(0).SetBbb(5)
+	c.GetB().GetCc().Get(0).SetCcc("6")
+	c.GetB().GetCc().Append(&model.TestA{})
+	c.GetB().GetCc().Get(1).SetId(7)
+	c.GetB().GetCc().Get(1).SetAaa(8)
+	c.GetB().GetCc().Get(1).SetBbb(9)
+	c.GetB().GetCc().Get(1).SetCcc("10")
+	c.GetB().GetCc().Set(1, &model.TestA{})
+	c.GetB().GetCc().Get(0).SetId(11)
+	c.GetB().GetCc().Get(0).SetAaa(12)
+	c.GetB().GetCc().Get(0).SetBbb(13)
+	c.GetB().GetCc().Get(0).SetCcc("14")
+	c.GetB().SetDd(mdata.NewMMap[string, *model.TestA]())
+	c.GetB().GetDd().Set("c_b_d_1", &model.TestA{})
+	c.GetB().GetDd().Get("c_b_d_1").SetId(15)
+	c.GetB().GetDd().Get("c_b_d_1").SetAaa(16)
+	c.GetB().GetDd().Get("c_b_d_1").SetBbb(17)
+	c.GetB().GetDd().Get("c_b_d_1").SetCcc("18")
+	c.SetC(mdata.NewList[*model.TestB]())
+	c.GetC().Append(&model.TestB{})
+	c.GetC().Get(0).SetId(19)
+	c.GetC().Get(0).SetAa("20")
+	c.GetC().Get(0).SetBb(&model.TestA{})
+	c.GetC().Get(0).GetBb().SetAaa(21)
+	c.GetC().Get(0).GetBb().SetBbb(22)
+	c.GetC().Get(0).GetBb().SetCcc("23")
+	c.GetC().Get(0).SetCc(mdata.NewList[*model.TestA]())
+	c.GetC().Get(0).GetCc().Append(&model.TestA{})
+	c.GetC().Get(0).GetCc().Get(0).SetAaa(24)
+	c.GetC().Get(0).GetCc().Get(0).SetBbb(25)
+	c.GetC().Get(0).GetCc().Get(0).SetCcc("26")
+	c.GetC().Get(0).SetDd(mdata.NewMMap[string, *model.TestA]())
+	c.GetC().Get(0).GetDd().Set("c_c_0_d", &model.TestA{})
+	c.GetC().Get(0).GetDd().Get("c_c_0_d").SetId(110)
+	c.GetC().Get(0).GetDd().Get("c_c_0_d").SetAaa(111)
+	c.GetC().Get(0).GetDd().Get("c_c_0_d").SetBbb(112)
+	c.GetC().Get(0).GetDd().Get("c_c_0_d").SetCcc("113")
+	c.SetD(mdata.NewMMap[string, *model.TestB]())
+	c.GetD().Set("c_d", &model.TestB{})
+	c.GetD().Get("c_d").SetAa("27")
+	c.GetD().Get("c_d").SetBb(&model.TestA{})
+	c.GetD().Get("c_d").GetBb().SetAaa(28)
+	c.GetD().Get("c_d").GetBb().SetBbb(29)
+	c.GetD().Get("c_d").GetBb().SetCcc("30")
+	c.GetD().Get("c_d").SetCc(mdata.NewList[*model.TestA]())
+	c.GetD().Get("c_d").GetCc().Append(&model.TestA{})
+	c.GetD().Get("c_d").GetCc().Get(0).SetId(31)
+	c.GetD().Get("c_d").GetCc().Get(0).SetAaa(32)
+	c.GetD().Get("c_d").GetCc().Get(0).SetBbb(33)
+	c.GetD().Get("c_d").GetCc().Get(0).SetCcc("34")
+	c.GetD().Get("c_d").SetDd(mdata.NewMMap[string, *model.TestA]())
+	c.GetD().Get("c_d").GetDd().Set("c_d_d", &model.TestA{})
+	c.GetD().Get("c_d").GetDd().Get("c_d_d").SetId(35)
+	c.GetD().Get("c_d").GetDd().Get("c_d_d").SetAaa(36)
+	c.GetD().Get("c_d").GetDd().Get("c_d_d").SetBbb(37)
+	c.GetD().Get("c_d").GetDd().Get("c_d_d").SetCcc("38")
+
+	return c
+}
 func TestMongoLoadSave(t *testing.T) {
-	var x uint64 = math.MaxUint64 - 1
-	var y int64 = int64(x)
-	fmt.Println(x)
-	fmt.Println(y)
-
-	a := model.TestA{}
-	a.SetId(123)
-	a.SetA(111)
-	a.SetB(222)
-
-	b := model.TestB{}
-	b.SetId(456)
-	b.SetM("333")
-	b.SetN(&a)
-
-	b.SetC(mdata.NewList[*model.TestA]())
-	v1 := &model.TestA{}
-	v1.SetId(1)
-	v1.SetA(2)
-	v1.SetB(3)
-	b.GetC().Append(v1)
-	v2 := &model.TestA{}
-	v2.SetId(11)
-	v2.SetA(12)
-	v2.SetB(13)
-	b.GetC().Append(v2)
-	//b.GetC().Set(0, v)
-
-	b.SetD(mdata.NewMMap[string, *model.TestA]())
-	v11 := &model.TestA{}
-	v11.SetId(100)
-	v11.SetA(101)
-	v11.SetB(102)
-	b.GetD().Set("100", v11)
-	v22 := &model.TestA{}
-	v22.SetId(110)
-	v22.SetA(111)
-	v22.SetB(112)
-	b.GetD().Set("110", v22)
-	b.CleanDirty(true)
-
-	c := model.TestC{}
-	c.SetId(789)
-	c.SetX("444")
-	c.SetY(&b)
-
-	m1, e := bson.Marshal(&c)
+	c := getTestC(123)
+	fmt.Printf("c:%+v\n", c)
+	m1, e := bson.Marshal(c)
 	if e != nil {
 		panic(e)
 	}
@@ -85,17 +117,17 @@ func TestMongoLoadSave(t *testing.T) {
 	if e != nil {
 		panic(e)
 	}
-	n3.GetY().GetC().Get(0).SetB(99999)
+	n3.SetA("aaa")
 	fmt.Printf("n2:%+v\n", &n2)
 	fmt.Printf("n3:%+v\n", n3)
 
-	if false {
-		mongo_helper.Connect("") //todo 换成自己的mongo地址测试
+	if mongoUrl != "" {
+		mongo_helper.Connect(mongoUrl)
 		defer mongo_helper.Close()
 		col := mongo_helper.GetCol("test", "model")
 
 		filter := bson.M{"_id": c.GetId()}
-		_, err := col.ReplaceOne(context.TODO(), filter, &c, options.Replace().SetUpsert(true))
+		_, err := col.ReplaceOne(context.TODO(), filter, c, options.Replace().SetUpsert(true))
 		if err != nil {
 			t.Error(err)
 		}
@@ -104,18 +136,18 @@ func TestMongoLoadSave(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		fmt.Println(&zz)
+		fmt.Printf("n3:%+v\n", &zz)
 	}
 }
 func TestDirtyUpdate(t *testing.T) {
-	if false {
-		mongo_helper.Connect("") //todo 换成自己的mongo地址测试
+	if mongoUrl != "" {
+		mongo_helper.Connect(mongoUrl)
 		defer mongo_helper.Close()
 		col := mongo_helper.GetCol("test", "model")
 
-		filter := bson.M{"_id": 789}
-		update1 := bson.M{"y.d.100.b": 10222}
-		update2 := bson.M{"y.d.110.a": ""}
+		filter := bson.M{"_id": 123}
+		update1 := bson.M{"a": "c_a_new", "b.bb.ccc": "1103", "d.c_d.aa": "1027", "d.c_d.bb.bbb": 1029}
+		update2 := bson.M{"b.aa": ""}
 
 		v, err := col.UpdateOne(context.TODO(), filter, bson.M{"$set": update1, "$unset": update2}, options.Update().SetUpsert(true))
 		if err != nil {
@@ -127,6 +159,29 @@ func TestDirtyUpdate(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		fmt.Println(&zz)
+		fmt.Printf("n3:%+v\n", &zz)
+	}
+}
+func TestBuildDirty(t *testing.T) {
+	if mongoUrl != "" {
+		mongo_helper.Connect(mongoUrl)
+		defer mongo_helper.Close()
+		col := mongo_helper.GetCol("test", "model")
+
+		filter := bson.M{"_id": 123}
+		c := getTestC(123)
+		m := bson.M{}
+		c.BuildDirty(m, "")
+		v, err := col.UpdateOne(context.TODO(), filter, m, options.Update().SetUpsert(true))
+		if err != nil {
+			t.Error(err)
+		}
+		_ = v
+		zz := model.TestC{}
+		err = col.FindOne(context.TODO(), filter).Decode(&zz)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Printf("n3:%+v\n", &zz)
 	}
 }
